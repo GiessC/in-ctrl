eebb7b6c82695aa332a0ddf15796a1390305e863
Collin Giess <giesscollin@gmail.com>
2024-03-05 19:51:57 -0500

    Added real test to web

--- web/__tests__/app/page.test.tsx
+++ web/__tests__/app/page.test.tsx
@@ -1,5 +1,16 @@
+import Home from '@/app/page';
+import '@testing-library/jest-dom';
+import { act, render, screen } from '@testing-library/react';
+
 describe('Page', () => {
-    it('sample', () => {
-        expect(true).toBe(true);
+    it('renders', async () => {
+        // When
+        act(() => {
+            render(<Home />);
+        });
+        const sampleText = await screen.findByText('Get started by editing');
+
+        // Then
+        expect(sampleText).toBeInTheDocument();
     });
 });
