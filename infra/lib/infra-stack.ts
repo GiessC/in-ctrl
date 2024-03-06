5b8b1f8bd4e6c10c12c8f140b7abfb4e7f4f9cd2
Collin Giess <giesscollin@gmail.com>
2024-03-05 20:06:29 -0500

    Added real infra test

--- infra/lib/infra-stack.ts
+++ infra/lib/infra-stack.ts
@@ -1,9 +1,32 @@
-import { Stack, type StackProps } from 'aws-cdk-lib';
+import { RemovalPolicy, Stack, type StackProps } from 'aws-cdk-lib';
+import { Bucket, HttpMethods } from 'aws-cdk-lib/aws-s3';
 import { type Construct } from 'constructs';
 
 export class InfraStack extends Stack {
     constructor(scope: Construct, id: string, props?: StackProps) {
         super(scope, id, props);
+        this.CreateSampleS3Bucket(this, id);
+    }
+
+    private CreateSampleS3Bucket(scope: Construct, id: string) {
+        new Bucket(scope, `${id}-SampleBucket`, {
+            bucketName: `${id}-SampleBucket`.toLowerCase(),
+            cors: [
+                {
+                    allowedMethods: [
+                        HttpMethods.GET,
+                        HttpMethods.PUT,
+                        HttpMethods.POST,
+                        HttpMethods.DELETE,
+                        HttpMethods.HEAD,
+                    ],
+                    allowedOrigins: ['*'],
+                    allowedHeaders: ['*'],
+                    id: `${id}-SampleBucket-Cors`,
+                },
+            ],
+            autoDeleteObjects: true,
+            removalPolicy: RemovalPolicy.DESTROY,
+        });
     }
 }
-
